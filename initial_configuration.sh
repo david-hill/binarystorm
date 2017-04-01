9e774a0b9ecd7cffe1ad04116dde7a9909e9de1d
David Hill <dhill@zappa.orion>
2017-04-01 18:46:19 -0400

    Enabling/starting saslauthd:

--- initial_configuration.sh
+++ initial_configuration.sh
@@ -4,6 +4,7 @@
 yum install -y postfix bind cyrus-imapd
 systemctl enable postfix
 systemctl enable cyrus-imapd
+systemctl enable saslauthd
 systemctl start postfix
 systemctl start cyrus-imapd
-
+systemctl start saslauthd
