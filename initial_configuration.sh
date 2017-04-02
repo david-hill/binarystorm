6ff11ac6dd8a87b4f69969f3db40959c5c61dc53
David Hill <dhill@zappa.orion>
2017-04-02 09:43:45 -0400

    Adding new files.

--- initial_configuration.sh
+++ initial_configuration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-yum install -y postfix bind cyrus-imapd
+yum install -y postfix bind cyrus-imapd cyrus-sasl vim bind-utils
 systemctl enable postfix
 systemctl enable cyrus-imapd
 systemctl enable saslauthd
