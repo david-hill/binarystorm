cd198d9f420549487f146ce441b7c07e05a5269c
David Hill <dhill@zappa.orion>
2017-04-02 14:16:44 -0400

    firewall tweaks in initial_configuration.sh

--- initial_configuration.sh
+++ initial_configuration.sh
@@ -15,8 +15,9 @@ saslpasswd2 -c cyrus
 passwd cyrus
 
 firewall-cmd --permanent --zone=public --add-port=25/tcp
-firewall-cmd --permanent --zone=public --add-port=110/tcp
 firewall-cmd --permanent --zone=public --add-port=53/tcp
 firewall-cmd --permanent --zone=public --add-port=53/udp
+firewall-cmd --permanent --zone=public --add-port=80/tcp
+firewall-cmd --permanent --zone=public --add-port=110/tcp
 firewall-cmd --reload
 
