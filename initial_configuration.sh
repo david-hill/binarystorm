89a6f49dbdd6e7426b15cc572304626521b0e998
David Hill <dhill@zappa.orion>
2017-04-02 10:55:21 -0400

    bind and postfix configuration

--- initial_configuration.sh
+++ initial_configuration.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 
-yum install -y postfix bind cyrus-imapd cyrus-sasl vim bind-utils telnet
-systemctl enable postfix
+yum install -y postfix bind cyrus-imapd cyrus-sasl vim bind-utils telnet httpd
 systemctl enable cyrus-imapd
+systemctl enable httpd
+systemctl enable postfix
 systemctl enable saslauthd
-systemctl start postfix
 systemctl start cyrus-imapd
+systemctl start httpd
+systemctl start postfix
 systemctl start saslauthd
 
 saslpasswd2 -c cyrus
 passwd cyrus
+
+firewall-cmd --permanent --zone=public --add-port=25/tcp
+firewall-cmd --permanent --zone=public --add-port=110/tcp
+firewall-cmd --permanent --zone=public --add-port=53/tcp
+firewall-cmd --permanent --zone=public --add-port=53/udp
+firewall-cmd --reload
+
