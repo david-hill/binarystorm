e6d30ed3a032acc4ebd74189295a4f8c5de161d5
David Hill <dhill@zappa.orion>
2017-04-02 10:16:46 -0400

    Fixing cyrus and postfix

--- initial_configuration.sh
+++ initial_configuration.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 
-yum install -y postfix bind cyrus-imapd cyrus-sasl vim bind-utils
+yum install -y postfix bind cyrus-imapd cyrus-sasl vim bind-utils telnet
 systemctl enable postfix
 systemctl enable cyrus-imapd
 systemctl enable saslauthd
 systemctl start postfix
 systemctl start cyrus-imapd
 systemctl start saslauthd
+
+saslpasswd2 -c cyrus
+passwd cyrus
