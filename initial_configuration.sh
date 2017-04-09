86f22b347dcc84fec73138aad159e4c78d03b04c
David Hill <dhill@zappa.orion>
2017-04-09 09:42:59 -0400

    Setting hostname and setting ntpd.

--- initial_configuration.sh
+++ initial_configuration.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+hostnamectl set-hostname binarystorm.net
 
-yum install -y postfix bind cyrus-imapd cyrus-sasl vim bind-utils telnet httpd
+
+yum install -y postfix bind cyrus-imapd cyrus-sasl vim bind-utils telnet httpd ntp
 systemctl enable cyrus-imapd
 systemctl enable httpd
 systemctl enable postfix
 systemctl enable saslauthd
+systemctl enable ntpd
 systemctl start cyrus-imapd
 systemctl start httpd
 systemctl start postfix
 systemctl start saslauthd
+systemctl start ntpd
 
 saslpasswd2 -c cyrus
 passwd cyrus
@@ -24,4 +28,5 @@ firewall-cmd --permanent --zone=public --add-port=53/udp
 firewall-cmd --permanent --zone=public --add-port=80/tcp
 firewall-cmd --permanent --zone=public --add-port=110/tcp
 firewall-cmd --reload
+
 
