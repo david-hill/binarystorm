f73b5e529a4b6f905861908fd15d16f1cda2c655
David Hill <dhill@zappa.orion>
2017-04-15 10:32:39 -0400

    Adding new configurations.

--- initial_configuration.sh
+++ initial_configuration.sh
@@ -3,17 +3,19 @@
 hostnamectl set-hostname binarystorm.net
 
 
-yum install -y postfix bind cyrus-imapd cyrus-sasl vim bind-utils telnet httpd ntp
+yum install -y postfix bind cyrus-imapd cyrus-sasl vim bind-utils telnet httpd ntp wget
 systemctl enable cyrus-imapd
 systemctl enable httpd
 systemctl enable postfix
 systemctl enable saslauthd
 systemctl enable ntpd
+systemctl enable named
 systemctl start cyrus-imapd
 systemctl start httpd
 systemctl start postfix
 systemctl start saslauthd
 systemctl start ntpd
+systemctl start named
 
 saslpasswd2 -c cyrus
 passwd cyrus
@@ -29,4 +31,7 @@ firewall-cmd --permanent --zone=public --add-port=80/tcp
 firewall-cmd --permanent --zone=public --add-port=110/tcp
 firewall-cmd --reload
 
-
+wget https://copr-be.cloud.fedoraproject.org/results/vrusinov/vrusinov/epel-7-x86_64/uptimed-0.4.0-1.fc21/uptimed-0.4.0-1.el7.centos.x86_64.rpm
+rpm -i uptimed*
+systemctl enable uptimed
+systemctl start uptimed
