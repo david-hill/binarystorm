dda256776993b908c8ebb50d1ffd49ebde210583
root <root@dns1.binarystorm.net>
2024-12-07 14:17:32 -0500

    A bunch of sudos are added here and there ... how do we add a sudo to a cat < "EOF" > /etc ?

--- upgrade_centos_7_to_centos_9_stream.sh
+++ upgrade_centos_7_to_centos_9_stream.sh
@@ -1,11 +1,10 @@
 sudo dnf update -y
-mkdir /root/backup
-sudo mv /etc/yum.repos.d/*.repo /root/backup
-dnf install http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/{centos-linux-repos-8-3.el8.noarch.rpm,centos-linux-release-8.5-1.2111.el8.noarch.rpm,centos-gpg-keys-8-3.el8.noarch.rpm}
-dnf -y upgrade https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-dnf clean all
-rpm -e `rpm -q kernel`
-rpm -e --nodeps sysvinit-tools
+sudo mkdir /root/backup_7
+sudo mv /etc/yum.repos.d/*.repo /root/backup_7
+sudo dnf install http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/{centos-linux-repos-8-3.el8.noarch.rpm,centos-linux-release-8.5-1.2111.el8.noarch.rpm,centos-gpg-keys-8-3.el8.noarch.rpm}
+sudo dnf clean all
+sudo rpm -e `rpm -q kernel`
+sudo rpm -e --nodeps sysvinit-tools
 
 cat << EOF > /etc/yum.repos.d/centos.repo
 [appstream]
@@ -41,6 +40,8 @@ sudo dnf -y groupupdate "Core" "Minimal Install"
 sudo dnf -y yum install kernel cloud-init vim
 sudo dnf -y install centos-release-stream
 sudo dnf -y swap centos-{linux,stream}-repos
+sudo mkdir /root/backup_8
+sudo mv /etc/yum.repos.d/*.repo /root/backup_8
 cat << EOF > /etc/yum.repos.d/centos.repo
 [Stream-AppStream]
 name=CentOS-Stream - AppStream
@@ -70,5 +71,6 @@ gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 EOF
 sudo dnf distro-sync
 
-dnf install https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-22.el9.noarch.rpm https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-22.el9.noarch.rpm https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-22.el9.noarch.rpm
-dnf --releasever=9 --allowerasing --setopt=deltarpm=false distro-sync -y
+sudo dnf install https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-22.el9.noarch.rpm https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-22.el9.noarch.rpm https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-22.el9.noarch.rpm
+sudo dnf --releasever=9 --allowerasing --setopt=deltarpm=false distro-sync -y
+sudo dnf -y upgrade https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
