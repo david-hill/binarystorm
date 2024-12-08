8cfb5461c1720e9ba2c4c669bd00f9604c64263c
root <root@dns1.binarystorm.net>
2024-12-08 10:48:16 -0500

    Added compatibility on etc/postfix/main.cf and fixed upgrade_centos_7_to_centos_9_stream.sh

--- upgrade_centos_7_to_centos_9_stream.sh
+++ upgrade_centos_7_to_centos_9_stream.sh
@@ -1,3 +1,4 @@
+set -e
 sudo dnf update -y
 sudo mkdir /root/backup_7
 sudo mv /etc/yum.repos.d/*.repo /root/backup_7
@@ -5,8 +6,7 @@ sudo dnf install http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/{cen
 sudo dnf clean all
 sudo rpm -e `rpm -q kernel`
 sudo rpm -e --nodeps sysvinit-tools man-pages
-
-cat << EOF > /etc/yum.repos.d/centos.repo
+sudo cat << EOF > /etc/yum.repos.d/centos.repo
 [appstream]
 name=CentOS Linux $releasever - AppStream
 baseurl=http://vault.centos.org/8.5.2111/AppStream/x86_64/os/
@@ -33,7 +33,6 @@ enabled=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 EOF
 sudo rpm -e nss nss-pem nss-sysinit nss-tools
-
 sudo dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync --disablerepo=appstream
 sudo dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync
 sudo dnf -y groupupdate "Core" "Minimal Install"
@@ -42,7 +41,7 @@ sudo dnf -y install centos-release-stream
 sudo dnf -y swap centos-{linux,stream}-repos
 sudo mkdir /root/backup_8
 sudo mv /etc/yum.repos.d/*.repo /root/backup_8
-cat << EOF > /etc/yum.repos.d/centos.repo
+sudo cat << EOF > /etc/yum.repos.d/centos.repo
 [Stream-AppStream]
 name=CentOS-Stream - AppStream
 #mirrorlist=https://vault.centos.org/centos/8-stream/AppStream/x86_64/os
@@ -74,10 +73,14 @@ sudo dnf distro-sync
 sudo mkdir /root/backup_8_stream
 sudo mv /etc/yum.repos.d/*.repo /root/backup_8_stream
 
-rpm -e --nodeps man-pages-fr iptables-ebtables
+sudo rpm -e --nodeps man-pages-fr iptables-ebtables
 
 sudo dnf install -y https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-22.el9.noarch.rpm https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-22.el9.noarch.rpm https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-22.el9.noarch.rpm
 sudo dnf --releasever=9 --allowerasing --setopt=deltarpm=false distro-sync -y
 sudo dnf -y upgrade https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 sudo rpm --rebuilddb
-sudo dnf -y install spamassin amavis
+sudo dnf -y install spamassin amavis vim fail2ban git bind bind-utils
+sudo cp /etc/named.conf.rpmsave /etc/named.conf
+sudo mkdir /var/lib/razor
+sudo systemctl enable amavisd fail2ban spamassassin named
+sudo systemctl start amavisd fail2ban spamassassin named
