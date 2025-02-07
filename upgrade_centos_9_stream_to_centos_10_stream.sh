feb0fa2b16768b85f90a00a27bff2aaa8cff4f7b
root <root@dns2.binarystorm.net>
2025-02-07 14:57:34 -0500

    Add upgrade_centos_9_stream_to_centos_10_stream.sh

---
+++ upgrade_centos_9_stream_to_centos_10_stream.sh
@@ -0,0 +1,37 @@
+cat<<EOF>/etc/yum.repos.d/centos.repo
+[baseos]
+name=CentOS Stream $releasever - BaseOS
+metalink=https://mirrors.centos.org/metalink?repo=centos-baseos-10-stream&arch=$basearch&protocol=https,http
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+gpgcheck=1
+repo_gpgcheck=0
+metadata_expire=6h
+countme=1
+enabled=1
+
+[appstream]
+name=CentOS Stream $releasever - AppStream
+metalink=https://mirrors.centos.org/metalink?repo=centos-appstream-10-stream&arch=$basearch&protocol=https,http
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+gpgcheck=1
+repo_gpgcheck=0
+metadata_expire=6h
+countme=1
+enabled=1
+EOF
+
+cat<<EOF>/etc/yum.repos.d/epel.repo
+[epel]
+name=Extra Packages for Enterprise Linux 10 - $basearch
+# It is much more secure to use the metalink, but if you wish to use a local mirror
+# place its address here.
+#baseurl=https://download.example/pub/epel/10/Everything/$basearch/
+metalink=https://mirrors.fedoraproject.org/metalink?repo=epel-10&arch=$basearch&infra=$infra&content=$contentdir
+enabled=1
+gpgcheck=1
+countme=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-9
+EOF
+
+yum clean all
+yum update -y
