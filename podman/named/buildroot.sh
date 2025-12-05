4f94d23123684ae6576602855daea6ec7f2ba090
root <root@dns3.binarystorm.net>
2025-12-05 21:52:29 +0100

    Rename root-named to named-root container image name

--- podman/named/buildroot.sh
+++ podman/named/buildroot.sh
@@ -2,6 +2,6 @@ source ../common/common.sh
 tmp=$(mktemp -d)
 yum install -y --nodocs --installroot=$tmp --nogpgcheck bind | tee install.out
 cleanup_root
-tar zcvf root-named.tgz -C $tmp .  | tee -a install.out
-podman import --change 'ENTRYPOINT ["/usr/sbin/named", "-f", "-u", "named", "-c", "/etc/named.conf"]' root-named.tgz root-named:$creation_date | tee -a install.out
+tar zcvf named-root.tgz -C $tmp .  | tee -a install.out
+podman import --change 'ENTRYPOINT ["/usr/sbin/named", "-f", "-u", "named", "-c", "/etc/named.conf"]' named-root.tgz named-root:$creation_date | tee -a install.out
 rm -rf $tmp
