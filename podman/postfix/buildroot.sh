c944e62ebfcb1a654361a9495a6e3d0ddd616921
root <root@dns3.binarystorm.net>
2025-12-05 21:50:29 +0100

    Add postfix entrypoint in buildroot.sh for postfix container.

--- podman/postfix/buildroot.sh
+++ podman/postfix/buildroot.sh
@@ -3,5 +3,5 @@ tmp=$(mktemp -d)
 yum install -y --installroot=$tmp --nogpgcheck postfix cyrus-imapd | tee install.out
 cleanup_root
 tar zcvf postfix-root.tgz -C $tmp . | tee -a install.out
-podman import postfix-root.tgz postfix-root:$creation_date | tee -a install.out
+podman import --change 'ENTRYPOINT ["/usr/sbin/postfix","start-fg"]' postfix-root.tgz postfix-root:$creation_date | tee -a install.out
 rm -rf $tmp
