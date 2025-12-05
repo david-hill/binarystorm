9b0fe4927c7e1e750c40e1bba9a7f195174cc5bc
root <root@dns3.binarystorm.net>
2025-12-05 21:30:32 +0100

    Add --change to podman import in  buildroot.sh to set entry point in named.

--- podman/named/buildroot.sh
+++ podman/named/buildroot.sh
@@ -3,5 +3,5 @@ tmp=$(mktemp -d)
 yum install -y --nodocs --installroot=$tmp --nogpgcheck bind | tee install.out
 cleanup_root
 tar zcvf root-named.tgz -C $tmp .  | tee -a install.out
-podman import root-named.tgz root-named:$creation_date | tee -a install.out
+podman import --change 'ENTRYPOINT ["/usr/sbin/named", "-f", "-u", "named", "-c", "/etc/named.conf"]' root-named.tgz root-named:$creation_date | tee -a install.out
 rm -rf $tmp
