f5b8d5ff9d6bb8147f9046715dca3e07a9779b4f
root <root@dns1.binarystorm.net>
2025-12-05 13:20:36 -0500

    Removing packages used to troubleshoot initial ipv6 issues and delete /var/log/dnf*

--- podman/named/buildroot.sh
+++ podman/named/buildroot.sh
@@ -1,10 +1,11 @@
 tmp=$(mktemp -d)
-yum install -y --nodocs --installroot=$tmp --nogpgcheck bind iproute net-tools tcpdump iputils | tee install.out
+yum install -y --nodocs --installroot=$tmp --nogpgcheck bind | tee install.out
 rm -rf $tmp/var/cache/dnf
 rm -rf $tmp/var/lib/dnf
 rm -rf $tmp/usr/share/man
 rm -rf $tmp/usr/share/doc
 rm -rf $tmp/usr/lib/.build-id
+rm -rf $tmp/var/log/dnf*
 tar zcvf root-named.tgz -C $tmp .  | tee -a install.out
 podman import root-named.tgz root-named:latest | tee -a install.out
 rm -rf $tmp
