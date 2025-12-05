a602a1298182498a81a83ee9712ea1a291950e8b
root <root@dns3.binarystorm.net>
2025-12-05 21:24:20 +0100

    Add common/common.sh and build container using current date as tag.

---
+++ podman/common/common.sh
@@ -0,0 +1,13 @@
+creation_date=$(date '+%Y-%m-%d')
+
+function cleanup_root {
+  if [ ! -z $tmp ]; then
+    rm -rf $tmp/var/cache/dnf
+    rm -rf $tmp/var/lib/dnf
+    rm -rf $tmp/usr/share/man
+    rm -rf $tmp/usr/share/doc
+    rm -rf $tmp/usr/lib/.build-id
+    rm -rf $tmp/var/log/dnf*
+    rm -rf $tmp/var/log/hawkey*
+  fi
+}
