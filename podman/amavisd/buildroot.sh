5f8cd9b5045f8e48e6d652173428cf6bedf29d80
root <root@dns2.binarystorm.net>
2025-12-10 09:14:32 -0500

    Fix buildroot.sh for amavisd and use amavisd-root:latest in start.sh

--- podman/amavisd/buildroot.sh
+++ podman/amavisd/buildroot.sh
@@ -3,5 +3,5 @@ tmp=$(mktemp -d)
 yum install -y --installroot=$tmp --nogpgcheck amavis perl-Razor-Agent pyzor | tee install.out
 cleanup_root
 tar zcvf amavisd-root.tgz -C $tmp . | tee -a install.out
-podman import amavisd-root.tgz amavisd-root:$creation_date | tee -a install.out
+podman import --change 'ENTRYPOINT ["/usr/sbin/amavisd","-c","/etc/amavisd/amavisd.conf","foreground"]' amavisd-root.tgz amavisd-root:$creation_date | tee -a install.out
 rm -rf $tmp
