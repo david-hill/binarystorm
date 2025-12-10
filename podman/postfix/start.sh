2ba894d144ec734443e7c0795e0efbedb4b8eebf
root <root@dns2.binarystorm.net>
2025-12-10 09:16:54 -0500

    Touch /etc/imapd.conf if it's not present.

--- podman/postfix/start.sh
+++ podman/postfix/start.sh
@@ -8,6 +8,9 @@ if [ $? -eq 0 ]; then
   podman stop postfix
   podman rm postfix
 fi
+if [ ! -e /etc/imapd.conf ]; then
+  touch /etc/imapd.conf
+fi
 podman  run -d --network ipv6 --ip6 fd00::3 --ip 10.89.0.3 -p 25:25/tcp -p [::]:25:25/tcp -h $(hostname) -v /etc/postfix:/etc/postfix:ro -v /var/spool/postfix:/var/spool/postfix -v /var/lib/imap/socket/lmtp:/var/lib/imap/socket/lmtp -v /etc/imapd.conf:/etc/imapd.conf:ro -v /var/lib/imap:/var/lib/imap -v /etc/aliases.lmdb:/etc/aliases.lmdb:ro --mount=type=bind,src=/dev/log,dst=/dev/log --name=postfix postfix-root:latest
 sleep 3
 podman generate systemd --new --files --name postfix
