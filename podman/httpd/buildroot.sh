a1749c9b5a8cc8bd690b9e6edcf7bff684a653d9
root <root@dns1.binarystorm.net>
2025-12-10 15:24:09 -0500

    Fix httpd entrypoint and start.sh

--- podman/httpd/buildroot.sh
+++ podman/httpd/buildroot.sh
@@ -3,5 +3,5 @@ tmp=$(mktemp -d)
 yum install -y --installroot=$tmp --nogpgcheck httpd httpd-core mod_ssl roundcubemail  | tee install.out
 cleanup_root
 tar zcvf httpd-root.tgz -C $tmp . | tee -a install.out
-podman import httpd-root.tgz httpd-root:$creation_date | tee -a install.out
+podman import --change 'ENTRYPOINT ["/usr/sbin/httpd","-DFOREGROUND"]' httpd-root.tgz httpd-root:$creation_date | tee -a install.out
 rm -rf $tmp
