15ab6dacef96ce1abaefbf098ab08e5f8be27c7f
root <root@dns1.binarystorm.net>
2025-12-10 15:29:04 -0500

    Add entrypoint for php-fpm and make fix start.sh

--- podman/php-fpm/buildroot.sh
+++ podman/php-fpm/buildroot.sh
@@ -4,5 +4,5 @@ packages="php-fpm php-pdo php-process php-gd php-intl php-ldap php-mbstring php-
 yum install -y --installroot=$tmp --nogpgcheck $packages | tee install.out
 cleanup_root
 tar zcvf php-fpm-root.tgz -C $tmp . | tee -a install.out
-podman import php-fpm-root.tgz php-fpm-root:$creation_date | tee -a install.out
+podman import --change 'ENTRYPOINT ["/usr/sbin/php-fpm","-F"]' php-fpm-root.tgz php-fpm-root:$creation_date | tee -a install.out
 rm -rf $tmp
