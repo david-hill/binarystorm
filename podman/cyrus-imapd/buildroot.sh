f8de5c7eea206b7d718b9e0a530be38e93405280
root <root@dns1.binarystorm.net>
2025-12-10 15:21:25 -0500

    Fixed cyrus-imapd entrypoint and start.sh

--- podman/cyrus-imapd/buildroot.sh
+++ podman/cyrus-imapd/buildroot.sh
@@ -3,5 +3,5 @@ tmp=$(mktemp -d)
 yum install -y --installroot=$tmp --nogpgcheck cyrus-imapd | tee install.out
 cleanup_root
 tar zcvf cyrus-imapd-root.tgz -C $tmp . | tee -a install.out
-podman import cyrus-imapd-root.tgz cyrus-imapd-root:$creation_date | tee -a install.out
+podman import --change 'ENTRYPOINT ["/usr/libexec/cyrus-imapd/master", "foreground"]' cyrus-imapd-root.tgz cyrus-imapd-root:$creation_date | tee -a install.out
 rm -rf $tmp
