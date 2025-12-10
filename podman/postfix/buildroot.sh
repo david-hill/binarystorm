969dd890b4a43f74395d1c36d5ddb54096001b01
root <root@dns2.binarystorm.net>
2025-12-10 14:28:43 -0500

    Install bind-utils in postfix, apparently we need that.

--- podman/postfix/buildroot.sh
+++ podman/postfix/buildroot.sh
@@ -1,6 +1,6 @@
 source ../common/common.sh
 tmp=$(mktemp -d)
-yum install -y --installroot=$tmp --nogpgcheck postfix cyrus-imapd | tee install.out
+yum install -y --installroot=$tmp --nogpgcheck postfix cyrus-imapd bind-utils | tee install.out
 cleanup_root
 tar zcvf postfix-root.tgz -C $tmp . | tee -a install.out
 podman import --change 'ENTRYPOINT ["/usr/sbin/postfix","start-fg"]' postfix-root.tgz postfix-root:$creation_date | tee -a install.out
