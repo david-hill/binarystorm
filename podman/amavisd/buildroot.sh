53b30f6d3a1fb8c1f731844508fa0b1dc1139639
root <root@dns2.binarystorm.net>
2025-12-10 20:47:09 -0500

    Add amavis to virusgroup and chgrp /run/clamd.scan to virusgroup

--- podman/amavisd/buildroot.sh
+++ podman/amavisd/buildroot.sh
@@ -1,6 +1,7 @@
 source ../common/common.sh
 tmp=$(mktemp -d)
 yum install -y --installroot=$tmp --nogpgcheck amavis perl-Razor-Agent pyzor | tee install.out
+chroot $tmp usermod -a -G virusgroup amavis  | tee -a install.out
 cleanup_root
 tar zcvf amavisd-root.tgz -C $tmp . | tee -a install.out
 podman import --change 'ENTRYPOINT ["/usr/sbin/amavisd","-c","/etc/amavisd/amavisd.conf","foreground"]' amavisd-root.tgz amavisd-root:$creation_date | tee -a install.out
--- podman/amavisd/start.sh
+++ podman/amavisd/start.sh
@@ -9,6 +9,7 @@ if [ $? -eq 0 ]; then
   podman rm amavisd
 fi
 chown 995 /var/spool/amavisd -R
+chgrp -R 998 /run/clamd.scan
 podman  run -d -h $(hostname) --network ipv6 --ip6 fd00::4 --ip 10.89.0.4 -v /etc/mail:/etc/mail:ro -v /usr/local/bin/dccproc:/usr/local/bin/dccproc:ro -v /var/dcc:/var/dcc -v /etc/amavisd:/etc/amavisd:ro -v /var/spool/amavisd:/var/spool/amavisd -v /var/lib/spamassassin:/var/lib/spamassassin:ro -v /var/run/clamd.scan:/var/run/clamd.scan --mount=type=bind,src=/dev/log,dst=/dev/log --name=amavisd amavisd-root:latest
 sleep 3
 podman generate systemd --new --files --name amavisd
