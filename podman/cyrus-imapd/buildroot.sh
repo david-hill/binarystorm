da0498909524ded57d30e4923e1a0815038568b6
root <root@dns1.binarystorm.net>
2025-12-11 12:39:35 -0500

    Added cyrus-sasl-plain rpm in cyrus-imapd and postfix container image.

--- podman/cyrus-imapd/buildroot.sh
+++ podman/cyrus-imapd/buildroot.sh
@@ -1,6 +1,6 @@
 source ../common/common.sh
 tmp=$(mktemp -d)
-yum install -y --installroot=$tmp --nogpgcheck cyrus-imapd | tee install.out
+yum install -y --installroot=$tmp --nogpgcheck cyrus-imapd cyrus-sasl-plain | tee install.out
 cleanup_root
 tar zcvf cyrus-imapd-root.tgz -C $tmp . | tee -a install.out
 podman import --change 'ENTRYPOINT ["/usr/libexec/cyrus-imapd/master", "foreground"]' cyrus-imapd-root.tgz cyrus-imapd-root:$creation_date | tee -a install.out
--- podman/postfix/buildroot.sh
+++ podman/postfix/buildroot.sh
@@ -1,6 +1,6 @@
 source ../common/common.sh
 tmp=$(mktemp -d)
-yum install -y --installroot=$tmp --nogpgcheck postfix cyrus-imapd bind-utils | tee install.out
+yum install -y --installroot=$tmp --nogpgcheck postfix cyrus-imapd bind-utils cyrus-sasl-plain | tee install.out
 cleanup_root
 tar zcvf postfix-root.tgz -C $tmp . | tee -a install.out
 podman import --change 'ENTRYPOINT ["/usr/sbin/postfix","start-fg"]' postfix-root.tgz postfix-root:$creation_date | tee -a install.out
