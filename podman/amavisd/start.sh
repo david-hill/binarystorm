4f69773adca874c605807d8e8d4e9eda7077b7e5
root <root@dns2.binarystorm.net>
2025-12-11 08:25:19 -0500

    Add template hosts file to amavisd and postfix containers.

--- podman/amavisd/start.sh
+++ podman/amavisd/start.sh
@@ -11,7 +11,7 @@ fi
 chown 995 /var/spool/amavisd -R
 chgrp 998 /var/lib/clamav -R
 chgrp -R 998 /run/clamd.scan
-podman  run -d -h $(hostname) --network ipv6 --ip6 fd00::4 --ip 10.89.0.4 -v /etc/mail:/etc/mail:ro -v /usr/local/bin/dccproc:/usr/local/bin/dccproc:ro -v /var/dcc:/var/dcc -v /etc/amavisd:/etc/amavisd:ro -v /var/spool/amavisd:/var/spool/amavisd -v /var/lib/spamassassin:/var/lib/spamassassin:ro -v /var/run/clamd.scan:/var/run/clamd.scan --mount=type=bind,src=/dev/log,dst=/dev/log --name=amavisd amavisd-root:latest
+podman  run -d -h $(hostname) --network ipv6 --ip6 fd00::4 --ip 10.89.0.4 -v /etc/mail:/etc/mail:ro -v /usr/local/bin/dccproc:/usr/local/bin/dccproc:ro -v /var/dcc:/var/dcc -v /etc/amavisd:/etc/amavisd:ro -v /var/spool/amavisd:/var/spool/amavisd -v /var/lib/spamassassin:/var/lib/spamassassin:ro -v /var/run/clamd.scan:/var/run/clamd.scan --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=amavisd amavisd-root:latest
 sleep 3
 podman generate systemd --new --files --name amavisd
 cp *.service /etc/systemd/system
