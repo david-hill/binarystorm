33b868f788430a84880c847bf1a2bde22b4259e5
root <root@dns1.binarystorm.net>
2025-12-16 11:34:10 -0500

    saslauthd need cyrus-imapd for pam depdencies (ugly) and ro access to /etc/passwd, /etc/shadow, etc

--- podman/saslauthd/buildroot.sh
+++ podman/saslauthd/buildroot.sh
@@ -1,7 +1,9 @@
 source ../common/common.sh
 tmp=$(mktemp -d)
-yum install -y --installroot=$tmp --nogpgcheck cyrus-sasl | tee install.out
+rpms="cyrus-sasl cyrus-imapd" 
+yum install -y --installroot=$tmp --nogpgcheck $rpms | tee install.out
 cleanup_root
-tar zcvf saslauthd -root.tgz -C $tmp . | tee -a install.out
+tar zcvf saslauthd-root.tgz -C $tmp . | tee -a install.out
 podman import --change 'ENTRYPOINT ["/usr/sbin/saslauthd","-d","-m","/run/saslauthd","-a","pam"]' saslauthd-root.tgz saslauthd-root:$creation_date | tee -a install.out
+podman tag saslauthd-root:$creation_date saslauthd-root:latest
 rm -rf $tmp
--- podman/saslauthd/start.sh
+++ podman/saslauthd/start.sh
@@ -8,8 +8,9 @@ if [ $? -eq 0 ]; then
   podman stop saslauthd
   podman rm saslauthd
 fi
+mkdir /run/saslauthd
 chmod 755 /run/saslauthd
-podman  run -d --network ipv6 --ip6 fd00::9 --ip 10.89.0.9 -h $(hostname) -v /etc/sasl2:/etc/sasl2:ro -v /etc/sasldb2:/etc/sasldb2:ro -v /run/saslauthd:/run/saslauthd --mount=type=bind,src=/dev/log,dst=/dev/log --name=saslauthd saslauthd-root:latest 
+podman  run -d --network ipv6 --ip6 fd00::9 --ip 10.89.0.9 -h $(hostname) -v /etc/passwd:/etc/passwd:ro -v /etc/shadow:/etc/shadow:ro -v /etc/gshadow:/etc/gshadow:ro -v /etc/sasl2:/etc/sasl2:ro -v /etc/sasldb2:/etc/sasldb2:ro -v /run/saslauthd:/run/saslauthd --mount=type=bind,src=/dev/log,dst=/dev/log --name=saslauthd saslauthd-root:latest 
 sleep 3
 podman generate systemd --new --files --name saslauthd
 cp *.service /etc/systemd/system
