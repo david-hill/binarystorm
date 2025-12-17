d86052c8860447414a568866802f270f1738728b
root <root@dns2.binarystorm.net>
2025-12-17 15:53:06 -0500

    Added sa-update container generation scripts and add sa-update to common/hosts

---
+++ podman/sa-update/start.sh
@@ -0,0 +1,17 @@
+systemctl | grep \ sa-update
+if [ $? -eq 0 ]; then
+  systemctl disable sa-update
+  systemctl stop sa-update
+fi
+podman ps -a | grep sa-update
+if [ $? -eq 0 ]; then
+  podman stop sa-update
+  podman rm sa-update
+fi
+podman  run -d -h $(hostname) --network ipv6 --ip6 fd00::11 --ip 10.89.0.11 -v /etc/mail:/etc/mail:ro -v /var/lib/spamassassin:/var/lib/spamassassin --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=sa-update sa-update-root:latest
+sleep 3
+podman generate systemd --new --files --name sa-update
+cp *.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable container-sa-update
+
