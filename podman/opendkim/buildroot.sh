e7be60e3774a80338699b25df3268adb6a8c51e9
root <root@dns2.binarystorm.net>
2025-12-18 10:14:04 -0500

    Switch path for opendkim .

--- podman/opendkim/buildroot.sh
+++ podman/opendkim/buildroot.sh
@@ -3,5 +3,5 @@ tmp=$(mktemp -d)
 yum install -y --installroot=$tmp --nogpgcheck opendkim | tee install.out
 cleanup_root
 tar zcvf opendkim-root.tgz -C $tmp . | tee -a install.out
-podman import --change 'ENTRYPOINT ["/usr/bin/opendkim","-f"]' opendkim-root.tgz opendkim-root:$creation_date | tee -a install.out
+podman import --change 'ENTRYPOINT ["/usr/sbin/opendkim","-f"]' opendkim-root.tgz opendkim-root:$creation_date | tee -a install.out
 rm -rf $tmp
