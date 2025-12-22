8c68e7cebf683a14a8178281c4fe296a1f1452f4
root <root@dns1.binarystorm.net>
2025-12-22 14:22:51 -0500

    Fix get_digest.sh and source conf to load $registry

--- podman/registry/get_digest.sh
+++ podman/registry/get_digest.sh
@@ -1,2 +1,4 @@
-curl -sS -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' \
-registry.davidchill.ca:5000/v2/$1/manifests/$2
+source conf
+#curl -v -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' \
+curl -s -H 'Accept: application/vnd.oci.image.manifest.v1+json' \
+$registry/v2/$1/manifests/$2 | jq .
