ad4cc65e2da950fd671bb440472b148b65100fdf
root <root@dns1.binarystorm.net>
2025-12-22 15:45:33 -0500

    Added conf and push_local_images_to_registry.sh

---
+++ podman/registry/push_local_images_to_registry.sh
@@ -0,0 +1,9 @@
+source conf
+for image in $( podman images | grep -v latest  | grep root  | awk '{ print $1 }' ); do
+   imagename=${image#*/}
+   tag=$( podman images | grep -v latest | grep root | grep $image | awk '{ print $2 }' | head -1)
+   echo "Pushing $image:$tag to $registry/$imagename:$tag"
+   podman push $image:$tag $registry/$imagename:$tag
+   echo "Pushing $image:$tag to $registry/$imagename:latest"
+   podman push $image:$tag $registry/$imagename:latest
+done
