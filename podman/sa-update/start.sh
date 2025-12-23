5e10a155d9f47b58206b80fb1fc1df16364823e9
root <root@dns3.binarystorm.net>
2025-12-23 15:50:22 +0100

    Use $registry in sa-update/start.sh

--- podman/sa-update/start.sh
+++ podman/sa-update/start.sh
@@ -1,3 +1,4 @@
+source ../common/common.sh
 systemctl | grep \ sa-update
 if [ $? -eq 0 ]; then
   systemctl disable sa-update
@@ -8,7 +9,7 @@ if [ $? -eq 0 ]; then
   podman stop sa-update
   podman rm sa-update
 fi
-podman  run -d -h $(hostname) --network ipv6 --ip6 fd00::11 --ip 10.89.0.11 -v /etc/mail:/etc/mail:ro -v /var/lib/spamassassin:/var/lib/spamassassin --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=sa-update sa-update-root:latest
+podman  run -d -h $(hostname) --network ipv6 --ip6 fd00::11 --ip 10.89.0.11 -v /etc/mail:/etc/mail:ro -v /var/lib/spamassassin:/var/lib/spamassassin --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=sa-update $registry/sa-update-root:latest
 sleep 3
 podman generate systemd --new --files --name sa-update
 cp *.service /etc/systemd/system
