55f3259265f4e5c39b44d337da70ce1d2f83578a
root <root@dns1.binarystorm.net>
2025-12-23 09:26:11 -0500

    Use the registry image in clamd/start.sh

--- podman/clamd/start.sh
+++ podman/clamd/start.sh
@@ -10,7 +10,7 @@ if [ $? -eq 0 ]; then
   podman rm clamd
 fi
 chown 995 /run/clamd.scan
-podman  run -d --network ipv6 --ip6 fd00::8 --ip 10.89.0.8 -h $(hostname) -v /etc/clamd.d:/etc/clamd.d:ro -v /var/run/clamd.scan/:/var/run/clamd.scan/ -v /var/spool/amavisd/tmp/:/var/spool/amavisd/tmp/ -v /var/lib/clamav:/var/lib/clamav:ro -v /run/clamd.amavisd:/run/clamd.amavisd --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=clamd clamd-root:latest
+podman  run -d --network ipv6 --ip6 fd00::8 --ip 10.89.0.8 -h $(hostname) -v /etc/clamd.d:/etc/clamd.d:ro -v /var/run/clamd.scan/:/var/run/clamd.scan/ -v /var/spool/amavisd/tmp/:/var/spool/amavisd/tmp/ -v /var/lib/clamav:/var/lib/clamav:ro -v /run/clamd.amavisd:/run/clamd.amavisd --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=clamd $registry/clamd-root:latest
 sleep 3
 podman generate systemd --new --files --name clamd
 cp *.service /etc/systemd/system
