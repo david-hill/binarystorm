86b3c1522e3ca4e3aa374b4c23bbd424208f63cf
root <root@dns3.binarystorm.net>
2025-12-23 16:09:25 +0100

    Load ../common/common.sh in opendkim/start.sh and use $registry to start the container.

--- podman/opendkim/start.sh
+++ podman/opendkim/start.sh
@@ -1,3 +1,4 @@
+source ../common/common.sh
 systemctl | grep opendkim
 if [ $? -eq 0 ]; then
 	systemctl disable opendkim
@@ -11,7 +12,7 @@ fi
 if [ ! -e /etc/imapd.conf ]; then
   touch /etc/imapd.conf
 fi
-podman  run -d --network ipv6 --ip6 fd00::12 --ip 10.89.0.12 -p 8891:8891/tcp -h $(hostname) -v /etc/opendkim.conf:/etc/opendkim.conf:ro -v /etc/opendkim:/etc/opendkim:ro --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=opendkim opendkim-root:latest
+podman  run -d --network ipv6 --ip6 fd00::12 --ip 10.89.0.12 -p 8891:8891/tcp -h $(hostname) -v /etc/opendkim.conf:/etc/opendkim.conf:ro -v /etc/opendkim:/etc/opendkim:ro --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=opendkim $registry/opendkim-root:latest
 sleep 3
 podman generate systemd --new --files --name opendkim
 cp *.service /etc/systemd/system
