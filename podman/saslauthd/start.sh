d0c9c0a2990b1a7f6f3e0a98cf0bfd35623493d2
root <root@dns3.binarystorm.net>
2025-12-23 16:07:41 +0100

    Create /run/saslauthd only if it's not created previously.

--- podman/saslauthd/start.sh
+++ podman/saslauthd/start.sh
@@ -9,7 +9,9 @@ if [ $? -eq 0 ]; then
   podman stop saslauthd
   podman rm saslauthd
 fi
-mkdir /run/saslauthd
+if [ ! -d /run/saslauthd ]; then
+  mkdir /run/saslauthd
+fi
 chmod 755 /run/saslauthd
 podman  run -d --network ipv6 --ip6 fd00::9 --ip 10.89.0.9 -h $(hostname) -v /etc/passwd:/etc/passwd:ro -v /etc/shadow:/etc/shadow:ro -v /etc/gshadow:/etc/gshadow:ro -v /etc/sasl2:/etc/sasl2:ro -v /etc/sasldb2:/etc/sasldb2:ro -v /run/saslauthd:/run/saslauthd --mount=type=bind,src=/dev/log,dst=/dev/log --name=saslauthd $registry/saslauthd-root:latest 
 sleep 3
