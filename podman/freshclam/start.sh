0474fd39012f82f6d77debdb3ec1cb924ab5d9ae
root <root@dns3.binarystorm.net>
2026-01-12 18:11:03 +0100

    Fix /var/lib/clamav permissions when starting freshclam

--- podman/freshclam/start.sh
+++ podman/freshclam/start.sh
@@ -11,6 +11,7 @@ if [ $? -eq 0 ]; then
   podman pull $registry/freshclam-root:latest
 fi
 chown 995 /run/clamd.scan
+chown 995 /var/lib/clamav
 podman run -d --network ipv6 --ip6 fd00::10 --ip 10.89.0.10 -h $(hostname) -v /etc/freshclam.conf:/etc/freshclam.conf:ro -v /var/lib/clamav:/var/lib/clamav --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=freshclam $registry/freshclam-root:latest
 sleep 3
 podman generate systemd --new --files --name freshclam
