2d8373652da4c6bf4b6bbe92e3834b33d92868ac
root <root@dns3.binarystorm.net>
2026-01-13 17:15:56 +0100

    Added compare_digest.sh

---
+++ podman/compare_digest.sh
@@ -0,0 +1,15 @@
+rpm -qi skopeo > /dev/null
+if [ $? -ne 0 ]; then
+  yum install -y skopeo
+fi
+
+remotesha=$(skopeo inspect docker://registry.davidchill.ca:5000/httpd-root:latest | jq .Digest)
+# "sha256:8858f5ccdb7f8acf6c7b8143612dbfc2d14dcba40ed6fafbab9fd3e1e6b8436b"
+localsha=$(podman inspect registry.davidchill.ca:5000/httpd-root:latest 2>/dev/null | jq .[]."Digest")
+# "sha256:8858f5ccdb7f8acf6c7b8143612dbfc2d14dcba40ed6fafbab9fd3e1e6b8436b"
+
+if [[ $remotesha == $localsha ]]; then
+	echo identical
+else
+	echo different
+fi
