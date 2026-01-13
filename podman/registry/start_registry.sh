b2af9e58f692aaf103bae3ba68c009e43977d5a6
root <root@dns1.binarystorm.net>
2026-01-13 11:12:59 -0500

    Use custom etc/distribution/config.yml and build a custom registry image to disable OTEL_TRACES_EXPORTER in the environment.

--- podman/registry/start_registry.sh
+++ podman/registry/start_registry.sh
@@ -1,5 +1,5 @@
 if [ ! -d /var/lib/registry ]; then
   mkdir /var/lib/registry
 fi
-podman run -d --name registry -p 5000:5000 -v /var/lib/registry:/var/lib/registry --restart=always registry:latest
+podman run -d --name registry -p 5000:5000 -v /etc/distribution/config.yml:/etc/distribution/config.yml:ro -v /var/lib/registry:/var/lib/registry --restart=always registry:latest
 
