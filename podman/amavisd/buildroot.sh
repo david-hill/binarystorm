38e5e7089957b8cbf868bcef2b581db9efffccb7
root <root@dns1.binarystorm.net>
2026-01-14 11:18:15 -0500

    Install dcc RPM in the amavisd container and remove bind mount volume of dcc binary.

--- podman/amavisd/buildroot.sh
+++ podman/amavisd/buildroot.sh
@@ -1,6 +1,6 @@
 source ../common/common.sh
 tmp=$(mktemp -d)
 service=amavisd
-packages="amavis perl-Razor-Agent pyzor"
+packages="amavis perl-Razor-Agent pyzor dcc"
 entrypoint='ENTRYPOINT ["/usr/sbin/amavisd","-c","/etc/amavisd/amavisd.conf","foreground"]'
 build_container
