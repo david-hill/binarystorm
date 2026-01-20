12b97e9d17b7a58f8e90250cc81c6e9f11cc81f9
root <root@dns1.binarystorm.net>
2026-01-20 09:11:47 -0500

    Rebuild rpm only if a new version is detected.

--- podman/builddcc/buildroot.sh
+++ podman/builddcc/buildroot.sh
@@ -1,6 +1,8 @@
-source ../common/common.sh
-packages="gcc curl tar rpm-build vim procps-ng perl-interpreter"
-service=builddcc
-entrypoint='ENTRYPOINT ["/bin/bash", "/builddcc.sh"]'
-#entrypoint='ENTRYPOINT ["/bin/bash"]'
-build_container
+if bash check_new_version.sh; then
+  source ../common/common.sh
+  packages="gcc curl tar rpm-build vim procps-ng perl-interpreter"
+  service=builddcc
+  entrypoint='ENTRYPOINT ["/bin/bash", "/builddcc.sh"]'
+#  entrypoint='ENTRYPOINT ["/bin/bash"]'
+  build_container
+fi
