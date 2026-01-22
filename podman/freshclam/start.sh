7f933be3b4a8d4e1673027a3479207988435d67b
David Hill <hostmaster@davidchill.ca>
2026-01-22 20:20:19 +0000

    Precreate freshclam directories.

--- podman/freshclam/start.sh
+++ podman/freshclam/start.sh
@@ -10,9 +10,13 @@ if [ $? -eq 0 ]; then
   podman rm freshclam
   podman pull $registry/freshclam-root:latest
 fi
+directories="/var/run/clamd.scan /var/spool/amavisd/tmp /var/lib/clamav /run/clamd.amavisd"
+for directory in $directories; do
+  mkdir -p $directory
+done
 chown 995 /run/clamd.scan
 chown 995 /var/lib/clamav
-podman run -d --network ipv6 --ip6 fd00::10 --ip 10.89.0.10 -h $(hostname) -v /etc/freshclam.conf:/etc/freshclam.conf:ro -v /var/lib/clamav:/var/lib/clamav --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=freshclam $registry/freshclam-root:latest
+podman run -d --network ipv6 --ip6 fd00::10 --ip 10.89.0.10 -h $(hostname) -v /root/binarystorm/etc/freshclam.conf:/etc/freshclam.conf:ro -v /var/lib/clamav:/var/lib/clamav --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=freshclam $registry/freshclam-root:latest
 sleep 3
 podman generate systemd --new --files --name freshclam
 cp *.service /etc/systemd/system
