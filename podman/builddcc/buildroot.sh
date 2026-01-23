410a7ecee60c50277a4d830c8c309fd14bee6080
David Hill <hostmaster@davidchill.ca>
2026-01-23 14:55:03 +0000

    Fix buildroot.sh

--- podman/builddcc/buildroot.sh
+++ podman/builddcc/buildroot.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 if bash /root/binarystorm/podman/builddcc/check_new_version.sh; then
   source /root/binarystorm/podman/common/common.sh
   packages="gcc curl tar rpm-build vim procps-ng perl-interpreter"
@@ -7,11 +6,3 @@ if bash /root/binarystorm/podman/builddcc/check_new_version.sh; then
 #  entrypoint='ENTRYPOINT ["/bin/bash"]'
   build_container
 fi
-=======
-source ../common/common.sh
-packages="gcc curl tar rpm-build vim procps-ng perl-interpreter"
-service=builddcc
-entrypoint='ENTRYPOINT ["/bin/bash", "/builddcc.sh"]'
-#entrypoint='ENTRYPOINT ["/bin/bash"]'
-build_container
->>>>>>> cc37894 (Adding dcc RPMS building scripts.)
