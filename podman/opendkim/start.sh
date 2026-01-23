f01ca55b06a7c2759bd5cea2a5446c9870c2333b
David Hill <hostmaster@davidchill.ca>
2026-01-23 05:32:34 +0000

    Fix /etc/opendkim/keys permissions in opendkim/start.sh and use /root/binarystorm/etc.

--- podman/opendkim/start.sh
+++ podman/opendkim/start.sh
@@ -10,10 +10,17 @@ if [ $? -eq 0 ]; then
   podman rm opendkim
   podman pull $registry/opendkim-root:latest
 fi
-if [ ! -e /etc/imapd.conf ]; then
-  touch /etc/imapd.conf
-fi
-podman  run -d --network ipv6 --ip6 fd00::12 --ip 10.89.0.12 -p 8891:8891/tcp -h $(hostname) -v /etc/opendkim.conf:/etc/opendkim.conf:ro -v /etc/opendkim:/etc/opendkim:ro --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=opendkim $registry/opendkim-root:latest
+chgrp -R 998 /etc/opendkim/keys
+podman  run -d --network ipv6 --ip6 fd00::12 --ip 10.89.0.12 -p 8891:8891/tcp -h $(hostname) \
+-v /root/binarystorm/etc/opendkim.conf:/etc/opendkim.conf:ro \
+-v /etc/opendkim/keys:/etc/opendkim/keys:ro \
+-v /root/binarystorm/etc/opendkim/TrustedHosts:/etc/opendkim/TrustedHosts:ro \
+-v /root/binarystorm/etc/opendkim/SigningTable:/etc/opendkim/SigningTable:ro \
+-v /root/binarystorm/etc/opendkim/KeyTable:/etc/opendkim/KeyTable:ro \
+--mount=type=bind,src=/dev/log,dst=/dev/log \
+--hosts-file ../common/hosts \
+--name=opendkim \
+$registry/opendkim-root:latest
 sleep 3
 podman generate systemd --new --files --name opendkim
 cp *.service /etc/systemd/system
