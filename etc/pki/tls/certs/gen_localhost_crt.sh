6158cd1d9f2fdbf91e4ad0e5d90e971250ecd8d2
David Hill <hostmaster@davidchill.ca>
2026-01-23 18:57:04 +0000

    Add full path in gen_localhost_crt.sh

--- etc/pki/tls/certs/gen_localhost_crt.sh
+++ etc/pki/tls/certs/gen_localhost_crt.sh
@@ -1,2 +1,2 @@
-openssl req -x509 -newkey rsa:4096 -keyout ../private/localhost.key -out localhost.crt -sha256 -days 3650 -nodes 
+openssl req -x509 -newkey rsa:4096 -keyout /etc/pki/tls/private/localhost.key -out /etc/pki/tls/certs/localhost.crt -sha256 -days 3650 -nodes 
 
