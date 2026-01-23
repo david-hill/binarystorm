4994945abad27fbf0285b7b2c176ea502b49afa9
David Hill <hostmaster@davidchill.ca>
2026-01-23 05:34:18 +0000

    php-fpm need access to roundcubemail files from httpd container, easy way here is to install the rpm in the php-fpm container image and mount the configuration file.

--- podman/php-fpm/buildroot.sh
+++ podman/php-fpm/buildroot.sh
@@ -1,4 +1,4 @@
 source /root/binarystorm/podman/common/common.sh
-packages="php-fpm php-pdo php-process php-gd php-intl php-ldap php-mbstring php-xml php-pecl-zip php-enchant"
+packages="roundcubemail php-fpm php-pdo php-process php-gd php-intl php-ldap php-mbstring php-xml php-pecl-zip php-enchant"
 service=php-fpm
 build_container
