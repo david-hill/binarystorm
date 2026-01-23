71949a5f0f96d03cf1f445b93995acfd5b3e2222
David Hill <hostmaster@davidchill.ca>
2026-01-23 05:30:13 +0000

    Create postfix directory tree and use /root/binarystorm/etc/postfix in start.sh

--- podman/postfix/start.sh
+++ podman/postfix/start.sh
@@ -10,13 +10,26 @@ if [ $? -eq 0 ]; then
   podman rm postfix
   podman pull $registry/postfix-root:latest
 fi
-if [ ! -e /etc/imapd.conf ]; then
-  touch /etc/imapd.conf
-fi
-if [ ! -e /var/lib/imap/socket/lmtp ]; then
-  mkdir -p /var/lib/imap/socket/lmtp
-fi
-podman  run -d --network ipv6 --ip6 fd00::3 --ip 10.89.0.3 -p 25:25/tcp -p [::]:25:25/tcp -p 587:587/tcp -p [::]:587:587/tcp -h $(hostname) -v /etc/postfix:/etc/postfix:ro -v /var/spool/postfix:/var/spool/postfix -v /var/lib/imap/socket/lmtp:/var/lib/imap/socket/lmtp -v /etc/imapd.conf:/etc/imapd.conf:ro -v /var/lib/imap:/var/lib/imap -v /etc/aliases.lmdb:/etc/aliases.lmdb:ro -v /etc/sasl2:/etc/sasl2:ro -v /run/saslauthd:/run/saslauthd --mount=type=bind,src=/dev/log,dst=/dev/log --hosts-file ../common/hosts --name=postfix $registry/postfix-root:latest
+directories="active bounce corrupt defer deferred flush hold incoming maildrop pid private public saved trace"
+for directory in $directories; do
+  mkdir -p /var/spool/postfix/$directory
+done
+chown -R 89:90 /var/spool/postfix
+podman  run -d --network ipv6 --ip6 fd00::3 --ip 10.89.0.3 -p 25:25/tcp -p [::]:25:25/tcp -p 587:587/tcp -p [::]:587:587/tcp -h $(hostname) \
+-v /root/binarystorm/etc/postfix/main.cf:/etc/postfix/main.cf:ro \
+-v /root/binarystorm/etc/postfix/master.cf:/etc/postfix/master.cf:ro \
+-v /root/binarystorm/etc/postfix/virtual.lmdb:/etc/postfix/virtual.lmdb:ro \
+-v /etc/postfix/keys:/etc/postfix/keys:ro \
+-v /var/spool/postfix:/var/spool/postfix \
+-v /root/binarystorm/etc/imapd.conf:/etc/imapd.conf:ro \
+-v /var/lib/imap:/var/lib/imap \
+-v /root/binarystorm/etc/aliases.lmdb:/etc/aliases.lmdb:ro \
+-v /root/binarystorm/etc/sasl2:/etc/sasl2:ro \
+-v /run/saslauthd:/run/saslauthd \
+--mount=type=bind,src=/dev/log,dst=/dev/log \
+--hosts-file ../common/hosts \
+--name=postfix \
+$registry/postfix-root:latest
 sleep 3
 podman generate systemd --new --files --name postfix
 cp *.service /etc/systemd/system
