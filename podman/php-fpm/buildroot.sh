bad2db08c9adb79869036b18381cfdc101fb43e5
David Hill <hostmaster@davidchill.ca>
2026-01-23 07:15:15 -0500

    Add roundcubemail package in php-fpm buildroot.sh

--- podman/php-fpm/buildroot.sh
+++ podman/php-fpm/buildroot.sh
@@ -1,4 +1,4 @@
 source /root/binarystorm/podman/common/common.sh
-packages="php-fpm php-pdo php-process php-gd php-intl php-ldap php-mbstring php-xml php-pecl-zip php-enchant"
+packages="php-fpm php-pdo php-process php-gd php-intl php-ldap php-mbstring php-xml php-pecl-zip php-enchant roundcubemail"
 service=php-fpm
 build_container
