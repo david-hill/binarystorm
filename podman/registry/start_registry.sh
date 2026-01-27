0b3eed0dc5ac75ae2a899ea61f7781064306eea5
David Hill <hostmaster@davidchill.ca>
2026-01-27 16:58:54 +0000

    Replace registry if it's already running.

--- podman/registry/start_registry.sh
+++ podman/registry/start_registry.sh
@@ -1,5 +1,5 @@
 if [ ! -d /var/lib/registry ]; then
   mkdir /var/lib/registry
 fi
-podman run -d --name registry -p 5000:5000 -v /root/binarystorm/etc/distribution/config.yml:/etc/distribution/config.yml:ro -v /var/lib/registry:/var/lib/registry --restart=always localhost/registry:latest
+podman run -d --replace --name registry -p 5000:5000 -v /root/binarystorm/etc/distribution/config.yml:/etc/distribution/config.yml:ro -v /var/lib/registry:/var/lib/registry --restart=always localhost/registry:latest
 
