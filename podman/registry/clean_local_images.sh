3b13079fedc092c52747dd45eafd77c59102a211
David Hill <hostmaster@davidchill.ca>
2026-01-30 14:23:07 +0000

    Improving clean_local_images.sh to delete Exited containers.

--- podman/registry/clean_local_images.sh
+++ podman/registry/clean_local_images.sh
@@ -1,1 +1,4 @@
+podman ps -a | grep Exited | awk '{ print $1 }' | xargs podman rm
 podman images | grep none | awk '{ print $3 }' | xargs podman rmi
+podman images | grep localhost | grep -v registry | awk '{ print $3 }' | xargs podman rmi
+podman images | grep registry.davidchill.ca | awk '{ print $3 }' | xargs podman rmi
