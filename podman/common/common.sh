ea4499a2546b8130a5459d720c808c2ee43586bf
David Hill <hostmaster@davidchill.ca>
2026-02-02 16:31:47 +0000

    Remove usr/lib/sysimage/rpm from container images.

--- podman/common/common.sh
+++ podman/common/common.sh
@@ -1,6 +1,6 @@
 creation_date=$(date '+%Y%m%d%H%M%S')
 tmp=$(mktemp -d)
-include="var/cache/dnf var/lib/dnf usr/share/man usr/share/doc usr/lib/.build-id var/log/dnf var/log/hawkey usr/share/licenses usr/share/X11 usr/lib/systemd usr/lib/rpm usr/lib/tmpfiles.d usr/share/dbus-1 etc/yum.repos.d etc/dnf etc/systemd etc/tpm2-tss run/systemd"
+include="var/cache/dnf var/lib/dnf usr/share/man usr/share/doc usr/lib/.build-id var/log/dnf var/log/hawkey usr/share/licenses usr/share/X11 usr/lib/systemd usr/lib/rpm usr/lib/tmpfiles.d usr/share/dbus-1 etc/yum.repos.d etc/dnf etc/systemd etc/tpm2-tss run/systemd usr/lib/sysimage/rpm"
 registry=registry.davidchill.ca:5000
 scriptlocation=$(echo $0 | xargs dirname)
 decompressargs='xf'
