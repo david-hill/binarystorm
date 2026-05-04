d932c790a1bafa3f39799788ae63eba25e141f19
David Hill <hostmaster@davidchill.ca>
2026-05-04 13:37:49 +0000

    Fix date variable usage in backup.sh

--- var/lib/imap/backup.sh
+++ var/lib/imap/backup.sh
@@ -1,12 +1,12 @@
 tdate=$(date "+%Y%m%d")
-if ! -e /var/spool/imap/mailbox-$date.tgz; then
+if ! -e /var/spool/imap/mailbox-$tdate.tgz; then
   cd  /var/spool/imap
-  tar zcvf mailbox-$date.tgz *
+  tar zcvf mailbox-$tdate.tgz *
   find /var/spool/imap/ -name mailbox\*tgz -mtime +7 -delete
 fi
 
-if ! -e /var/lib/imap/backup-$date.tgz; then
+if ! -e /var/lib/imap/backup-$tdate.tgz; then
   cd /var/lib/imap
-  tar zcvf backup-$date.tgz *
+  tar zcvf backup-$tdate.tgz *
   find /var/lib/imap/ -name backup\*tgz -mtime +7 -delete
 fi
