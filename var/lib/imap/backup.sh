e84bcf1090baee46089a77071d0a2045e848a2db
David Hill <hostmaster@davidchill.ca>
2026-05-04 14:01:39 +0000

    fix condition for backup creation in backup.sh

--- var/lib/imap/backup.sh
+++ var/lib/imap/backup.sh
@@ -1,11 +1,11 @@
 tdate=$(date "+%Y%m%d")
-if ! -e /var/spool/imap/mailbox-$tdate.tgz; then
+if ! [ -e /var/spool/imap/mailbox-$tdate.tgz ]; then
   cd  /var/spool/imap
   tar zcvf mailbox-$tdate.tgz *
   find /var/spool/imap/ -name mailbox\*tgz -mtime +7 -delete
 fi
 
-if ! -e /var/lib/imap/backup-$tdate.tgz; then
+if ! [ -e /var/lib/imap/backup-$tdate.tgz ]; then
   cd /var/lib/imap
   tar zcvf backup-$tdate.tgz *
   find /var/lib/imap/ -name backup\*tgz -mtime +7 -delete
