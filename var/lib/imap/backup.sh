bf48ff2cc039a689e1e02759338a7bdc88343456
David Hill <hostmaster@davidchill.ca>
2026-05-04 13:34:55 +0000

    rotate backup and keep only last 7 days

--- var/lib/imap/backup.sh
+++ var/lib/imap/backup.sh
@@ -1,4 +1,12 @@
-cd  /var/spool/imap
-tar zcvf mailbox.tgz *
-cd /var/lib/imap
-tar zcvf backup.tgz *
+tdate=$(date "+%Y%m%d")
+if ! -e /var/spool/imap/mailbox-$date.tgz; then
+  cd  /var/spool/imap
+  tar zcvf mailbox-$date.tgz *
+  find /var/spool/imap/ -name mailbox\*tgz -mtime +7 -delete
+fi
+
+if ! -e /var/lib/imap/backup-$date.tgz; then
+  cd /var/lib/imap
+  tar zcvf backup-$date.tgz *
+  find /var/lib/imap/ -name backup\*tgz -mtime +7 -delete
+fi
