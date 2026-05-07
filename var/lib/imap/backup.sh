f575c7b9d318330cb87db375d43d628bd80a235b
David Hill <hostmaster@davidchill.ca>
2026-05-07 15:03:03 +0000

    Do not include previous backup in current backup

--- var/lib/imap/backup.sh
+++ var/lib/imap/backup.sh
@@ -3,12 +3,12 @@
 tdate=$(date "+%Y%m%d")
 if ! [ -e /var/spool/imap/mailbox-$tdate.tgz ]; then
   cd  /var/spool/imap
-  tar zcvf mailbox-$tdate.tgz *
+  tar zcvf mailbox-$tdate.tgz --exclude='mailbox*.tgz' *
   find /var/spool/imap/ -name mailbox\*tgz -mtime +7 -delete
 fi
 
 if ! [ -e /var/lib/imap/backup-$tdate.tgz ]; then
   cd /var/lib/imap
-  tar zcvf backup-$tdate.tgz *
+  tar zcvf backup-$tdate.tgz --exclude='backup*.tgz' *
   find /var/lib/imap/ -name backup\*tgz -mtime +7 -delete
 fi
