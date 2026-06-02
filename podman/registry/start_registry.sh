7fd67dd48db64124cc5a675fb09d71902e12ecee
David Hill <hostmaster@davidchill.ca>
2026-06-02 14:54:53 +0000

    Add argument to start registry in ro or rw mode.

--- podman/registry/start_registry.sh
+++ podman/registry/start_registry.sh
@@ -1,5 +1,15 @@
+if [ "$#" -gt 0 ]; then
+  if [ $1 == "ro" ] || [ $1 == "rw" ]; then
+    permission=$1
+  else
+    permission="ro"
+  fi
+else
+  permission="ro"
+fi
+
 if [ ! -d /var/lib/registry ]; then
   mkdir /var/lib/registry
 fi
-podman run -d --replace --name registry -p 5000:5000 -v /root/binarystorm/etc/distribution/config.yml:/etc/distribution/config.yml:ro -v /var/lib/registry:/var/lib/registry --restart=always localhost/registry:latest
+podman run -d --replace --name registry -p 5000:5000 -v /root/binarystorm/etc/distribution/config.yml:/etc/distribution/config.yml:ro -v /var/lib/registry:/var/lib/registry:$permission --restart=always localhost/registry:latest
 
